15115f4fd743f982fbbe2b2adbf95b222ad40389
danischm <danischm@cisco.com>
2022-10-03 15:57:38 +0200

    Bump modules

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "interface_policies" {
 
 module "tenant" {
   source  = "netascode/nac-tenant/aci"
-  version = ">= 0.2.0"
+  version = ">= 0.2.1"
 
   for_each    = toset([for tenant in lookup(local.model.apic, "tenants", {}) : tenant.name])
   model       = local.model
