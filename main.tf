b9aabf4ec1fdf597908ac134dc85db5e2d535073
danischm <danischm@cisco.com>
2022-10-17 14:56:05 +0200

    Module updates

--- main.tf
+++ main.tf
@@ -29,28 +29,28 @@ data "utils_yaml_merge" "model" {
 
 module "access_policies" {
   source  = "netascode/nac-access-policies/aci"
-  version = ">= 0.2.0"
+  version = "0.3.0"
 
   model = local.model
 }
 
 module "fabric_policies" {
   source  = "netascode/nac-fabric-policies/aci"
-  version = ">= 0.2.0"
+  version = "0.3.0"
 
   model = local.model
 }
 
 module "pod_policies" {
   source  = "netascode/nac-pod-policies/aci"
-  version = ">= 0.1.1"
+  version = "0.3.0"
 
   model = local.model
 }
 
 module "node_policies" {
   source  = "netascode/nac-node-policies/aci"
-  version = ">= 0.2.0"
+  version = "0.3.0"
 
   model = local.model
 
@@ -59,7 +59,7 @@ module "node_policies" {
 
 module "interface_policies" {
   source  = "netascode/nac-interface-policies/aci"
-  version = ">= 0.2.0"
+  version = "0.3.0"
 
   for_each = { for node in lookup(lookup(local.model.apic, "interface_policies", {}), "nodes", []) : node.id => node }
   model    = local.model
@@ -70,7 +70,7 @@ module "interface_policies" {
 
 module "tenant" {
   source  = "netascode/nac-tenant/aci"
-  version = ">= 0.2.2"
+  version = "0.3.0"
 
   for_each    = toset([for tenant in lookup(local.model.apic, "tenants", {}) : tenant.name])
   model       = local.model
