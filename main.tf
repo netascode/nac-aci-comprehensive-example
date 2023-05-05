d075a9a63dc7870add0759406876526f1cc79c14
Rob van der Kind <r.vdkind@hotmail.com>
2023-05-05 10:16:54 +0200

    bump to nac-aci module

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     aci = {
       source  = "CiscoDevNet/aci"
-      version = ">= 2.6.0"
     }
   }
 }
@@ -13,67 +12,16 @@ provider "aci" {
   url      = "https://apic.url"
 }
 
-module "merge" {
-  source  = "netascode/nac-merge/utils"
-  version = "0.1.2"
+module "aci" {
+  source  = "netascode/nac-aci/aci"
+  version = "0.7.0"
 
-  yaml_strings = concat(
-    [for file in fileset(path.module, "data/*.yaml") : file(file)],
-    [file("${path.module}/defaults/defaults.yaml")],
-  )
-}
+  yaml_directories = ["data"]
 
-module "access_policies" {
-  source  = "netascode/nac-access-policies/aci"
-  version = "0.4.1"
-
-  model = module.merge.model
-}
-
-module "fabric_policies" {
-  source  = "netascode/nac-fabric-policies/aci"
-  version = "0.4.2"
-
-  model = module.merge.model
-}
-
-module "pod_policies" {
-  source  = "netascode/nac-pod-policies/aci"
-  version = "0.4.0"
-
-  model = module.merge.model
-}
-
-module "node_policies" {
-  source  = "netascode/nac-node-policies/aci"
-  version = "0.4.0"
-
-  model = module.merge.model
-
-  dependencies = [module.access_policies.critical_resources_done]
-}
-
-module "interface_policies" {
-  source  = "netascode/nac-interface-policies/aci"
-  version = "0.4.0"
-
-  for_each = { for node in try(module.merge.model.apic.interface_policies.nodes, []) : node.id => node }
-  model    = module.merge.model
-  node_id  = each.value.id
-
-  dependencies = [module.access_policies.critical_resources_done]
-}
-
-module "tenant" {
-  source  = "netascode/nac-tenant/aci"
-  version = "0.4.2"
-
-  for_each    = { for tenant in try(module.merge.model.apic.tenants, []) : tenant.name => tenant }
-  model       = module.merge.model
-  tenant_name = each.value.name
-
-  dependencies = [
-    module.access_policies.critical_resources_done,
-    module.fabric_policies.critical_resources_done,
-  ]
+  manage_access_policies    = true
+  manage_fabric_policies    = true
+  manage_pod_policies       = true
+  manage_node_policies      = true
+  manage_interface_policies = true
+  manage_tenants            = true
 }
